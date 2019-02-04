7c2d6ad2a5c0685f099a20a9e9169e5986ee389c
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-02-04 18:36:50 +0600

    Add files via upload

---
+++ readingFile.py
@@ -0,0 +1,27 @@
+
+
+
+with open("C:\\Users\Saif\Desktop\eclipse.txt",'r') as f:
+    for line in f:
+        for word in line.split(" : "):
+            print(word)
+
+
+
+
+
+
+
+
+#f = open("C:\\Users\Saif\Desktop\eclipse.txt",'r')
+
+#lines = f.readlines()
+
+#for i in lines:
+
+    #thisline = i.split(" ")
+   # print(thisline)
+
+#for x in f:
+
+   # print(x)
