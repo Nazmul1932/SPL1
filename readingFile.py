501f765d70258f28a88064a5a34f7529a2c173e6
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-02-08 10:58:54 +0600

    Update readingFile.py

--- readingFile.py
+++ readingFile.py
@@ -1,27 +1,12 @@
 
 
 
-with open("C:\\Users\Saif\Desktop\eclipse.txt",'r') as f:
-    for line in f:
-        for word in line.split(" : "):
-            print(word)
-
-
-
-
-
-
-
-
-#f = open("C:\\Users\Saif\Desktop\eclipse.txt",'r')
-
-#lines = f.readlines()
-
-#for i in lines:
-
-    #thisline = i.split(" ")
-   # print(thisline)
-
-#for x in f:
-
-   # print(x)
+# load text
+filename = 'mozila.txt'
+file = open(filename, 'rt')
+text = file.read()
+file.close()
+# split based on words only
+import re
+words = re.split(r'\W+', text)
+print(words[:])
