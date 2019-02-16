ce4f92e11e8503d020e807003f8256f012a61f5c
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-02-16 23:12:58 +0600

    Update readingFile.py

--- readingFile.py
+++ readingFile.py
@@ -1,30 +1,26 @@
 
 
 
-#mozila.txt
-#eclipse.txt
-
-
 import io
 from nltk.corpus import stopwords
 import re
 
 stop_words = set(stopwords.words('english'))
+
+
 # load text
-
-
 filename = 'mozila.txt'
 file = open(filename, 'rt')
 text = file.read()
 file.close()
 
+
 # split based on words only
 words = re.split(r'\W+', text)
-print(words[:])
+#print(words[:])
 
-for r in words:
-   if not r in stop_words:
-      print(r)
-
+for i in words:
+   if not i in stop_words:
+      print(i)
 
 
