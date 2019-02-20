343ab4c3f9beeb9ca364015987c7d24850df8def
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-02-20 23:11:27 +0600

    Update readingFile.py

--- readingFile.py
+++ readingFile.py
@@ -1,27 +1,29 @@
 
-
-
 from nltk.corpus import stopwords
 import re
 from collections import Counter
 
-stop_words = set(stopwords.words('english'))
-
-
 # load text
-filename = 'mozila.txt'
+filename = "mozila.txt"
 file = open(filename, 'rt')
 text = file.read()
 file.close()
 
-
 # split based on words only
 words = re.split(r'\W+', text)
-#print(words[:])
+# print(words[:])
+
+stopwords = stopwords.words('english')
+stop_words = set(stopwords)
 
 for i in words:
-   if not i in stop_words:
-      #print(i)
-      print(Counter(text.split()))
-      break
+    if not i in stop_words:
+        # print(i)
+        result: str = ''.join(j for j in text if not j.isdigit())
+        word = result.split()
+        count = Counter(word)
+        print(count)
+        break
+
+
 
