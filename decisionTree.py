de99016772bd8ee1fd415f1b4c5243a7548b61ab
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-05-25 13:50:21 +0600

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -1,168 +1,192 @@
 from csv import reader
+import math
 import random
 
 
-def load_CSV_file(file_name):
-	read_file = open(file_name)
-	read_lines = reader(read_file)
-	data_set = list(read_lines)
-	return data_set
+def count_class_wise_dist(Y, classes):
+    prob = [0] * len(classes)
+    if len(Y) == 0:
+        return prob
+
+    for i in range(classes):
+        class_val = classes[i]
+        prob[i] = Y.count(class_val) / len(Y)
+
+    return prob
 
 
-def str2float(dataset, column):
-	for row in dataset:
-		row[column] = float(row[column].strip())
+def claculate_entropy(prob):
+    num = len(prob)
+    entropy = 0.0
 
-def split_dataset(index, value, dataset):
-	left, right = list(), list()
-	for row in dataset:
-		if row[index] < value:
-			left.append(row)
-		else:
-			right.append(row)
-	#print(left)
-	#print(right)
-	return left, right
+    for i in range(len(prob)):
+        if prob[i] > 0:
+            entropy = entropy - (prob[i] * math.log2(prob[i]) / math.log2(num))
+    return entropy
 
 
-def find_Gini(groups, classes):
-	length_of_splitting_groups = float(sum([len(group) for group in groups]))
+def find_Gini(Y_tr, Y_left, Y_right):
+    classes = set(Y_tr)
 
-	weighted_Gini = 0.0
-	for group in groups:
-		group_size = float(len(group))
+    prob_tr = count_class_wise_dist(Y_tr, classes)
+    prob_left = count_class_wise_dist(Y_left, classes)
+    prob_right = count_class_wise_dist(Y_right, classes)
 
-		if group_size == 0:
-			continue
-		score = 0.0
+    parent_ent = claculate_entropy(prob_tr)
+    left_ent = claculate_entropy(prob_left)
+    right_ent = claculate_entropy(prob_right)
 
-		for class_values_of_dataset in classes:
-			probability = [row[-1] for row in group].count(class_values_of_dataset) / group_size
-			score += probability * probability
+    gini = parent_ent - left_ent*len(Y_left)/len(Y_tr) - right_ent*len(Y_right)/len(Y_tr)
 
-		weighted_Gini += (1.0 - score) * (group_size / length_of_splitting_groups)
-	return weighted_Gini
+    return gini
 
 
-def getting_Split_Dataset(dataset):
-	class_values_of_dataset = list(set(row[-1] for row in dataset))
-	n_index, n_value, n_score, n_groups = 1000, 1000, 1000, None
-	for index in range(len(dataset[0]) - 1):
-		for row in dataset:
-			groups = split_dataset(index, row[index], dataset)
-			#print(groups)
-			gini = find_Gini(groups, class_values_of_dataset)
-			#print(gini)
-			if gini < n_score:
-				n_index, n_value, n_score, n_groups = index, row[index], gini, groups
-	return {'index': n_index, 'value': n_value, 'groups': n_groups}
+def getting_Split_Dataset(X_tr, Y_tr):
+    unique_class = len(set(Y_tr))
+    if unique_class == 1:
+        return {'decision': True, 'result': Y_tr[0], 'index': '', 'value': ''}
+
+    max_gini = 1000
+    n_index = -1
+    n_value = -1
+
+    for index in range(len(X_tr[0])):
+        for row in X_tr:
+            _, _, Y_left, Y_right = split_dataset(index, row[index], X_tr, Y_tr)
+            if len(Y_right) == 0:
+                continue
+
+            gini = find_Gini(Y_tr, Y_left, Y_right)
+            if gini < max_gini:
+                n_index, n_value, n_score, n_groups = index, row[index], gini
+    return {'decision': False, 'result': '', 'index': n_index, 'value': n_value}
 
 
 def to_terminal(group):
-	outcomes = [row[-1] for row in group]
-	return max(set(outcomes), key=outcomes.count)
+    outcomes = [row[-1] for row in group]
+    return max(set(outcomes), key=outcomes.count)
 
 
 def make_TREE(node, max_depth, min_size, depth):
-	left, right = node['groups']
-	del (node['groups'])
+    left, right = node['groups']
+    del (node['groups'])
 
-	if not left or not right:
-		node['left'] = node['right'] = to_terminal(left + right)
-		return
+    if not left or not right:
+        node['left'] = node['right'] = to_terminal(left + right)
+        return
 
-	if depth >= max_depth:
-		node['left'], node['right'] = to_terminal(left), to_terminal(right)
-		return
+    if depth >= max_depth:
+        node['left'], node['right'] = to_terminal(left), to_terminal(right)
+        return
 
-	if len(left) <= min_size:
-		node['left'] = to_terminal(left)
-	else:
-		node['left'] = getting_Split_Dataset(left)
-		make_TREE(node['left'], max_depth, min_size, depth + 1)
+    if len(left) <= min_size:
+        node['left'] = to_terminal(left)
+    else:
+        node['left'] = getting_Split_Dataset(left)
+        make_TREE(node['left'], max_depth, min_size, depth + 1)
 
-	if len(right) <= min_size:
-		node['right'] = to_terminal(right)
-	else:
-		node['right'] = getting_Split_Dataset(right)
-		make_TREE(node['right'], max_depth, min_size, depth + 1)
-
-
-def build_decision_TREE(train, max_depth, min_size):
-	root = getting_Split_Dataset(train)
-	#print(root)
-	make_TREE(root, max_depth, min_size, 1)
-	return root
+    if len(right) <= min_size:
+        node['right'] = to_terminal(right)
+    else:
+        node['right'] = getting_Split_Dataset(right)
+        make_TREE(node['right'], max_depth, min_size, depth + 1)
 
 
-
-def predict(node, row):
-	if row[node['index']] < node['value']:
-		if isinstance(node['left'], dict):
-			return predict(node['left'], row)
-		else:
-			return node['left']
-	else:
-		if isinstance(node['right'], dict):
-			return predict(node['right'], row)
-		else:
-			return node['right']
+def build_decision_TREE(X_tr, Y_tr, max_depth, min_size):
+    root = getting_Split_Dataset(X_tr, Y_tr)
+    #print(root)
+    #build_decision_TREE(X_tr, Y_tr, max_depth, min_size)
+    return root
 
 
-def decision_tree(train,test, min_size, max_depth):
-	train_data_tree = build_decision_TREE(train, max_depth, min_size)
+def decision_tree(X_train, Y_train, min_size, max_depth):
+    tree = build_decision_TREE(X_train, Y_train, max_depth, min_size)
 
-	predictions = list()
-	for row in test:
 
-		prediction = predict(train_data_tree, row)
-		predictions.append(prediction)
+def load_CSV_file(file_name):
+    read_file = open(file_name)
+    read_lines = reader(read_file)
+    data_set = list(read_lines)
+    return data_set
 
-	return predictions
 
-'''
+def str2float(dataset, column):
+    for row in dataset:
+        row[column] = float(row[column].strip())
+
+
+def split_dataset(index, value, X, Y):
+    left = []
+    right = []
+    Y_left, Y_right = [], []
+
+    for i in len(X):
+        row = X[i]
+        if row[index] <= value:
+            left.append(row)
+            Y_left.append(Y[i])
+        else:
+            right.append(row)
+            Y_right.append(Y[i])
+
+    return left, right, Y_left, Y_right
+
+
 def shuffle_data(dataset):
-	length = len(dataset)
-	random.seed(30)
-	for i in range(length):
-		j = random.randint(1, length-1)
-		data = dataset[i]
-		dataset[i] = dataset[j]
-		dataset[j] = data
-	return dataset
-'''
+    length = len(dataset)
+    random.seed(30)
+    for i in range(length):
+        j = random.randint(1, length-1)
+        data = dataset[i]
+        dataset[i] = dataset[j]
+        dataset[j] = data
+    return dataset
+
 
 def train_test_split(data_set, index, k=10):
-	data_set = random.shuffle(data_set)
-	#print(str(data_set))
-	train = []
-	test = []
+    data_set = shuffle_data(data_set)
+    #print(str(data_set))
+    train = []
+    test = []
 
-	for i in range(len(data_set)):
-		if i % k == index:
-			test.append(data_set[i])
-		else:
-			train.append(data_set[i])
+    for i in range(len(data_set)):
+        if i % k == index:
+            test.append(data_set[i])
+        else:
+            train.append(data_set[i])
 
-	return train,test
+    return train,test
+
+
+def feature_class_split(dataset):
+    length = len(dataset)
+    fea_len = len(dataset[0])-1
+    x = []
+    y = []
+    for i in range(length):
+        x.append(dataset[i][:-1])
+        y.append(dataset[i][fea_len])
+
+    return x, y
+
 
 if __name__ == '__main__':
-	file_name = 'bank.csv'
-	data_set = load_CSV_file(file_name)
+    file_name = 'bank.csv'
+    data_set = load_CSV_file(file_name)
 
-	for i in range(len(data_set[0])):
-		str2float(data_set, i)
+    for i in range(len(data_set[0])):
+        str2float(data_set, i)
 
+    train, test = train_test_split(data_set, 6, k=10)
 
+    max_depth = 5
+    min_size = 10
 
-	X_tr, X_te = train_test_split(data_set, 6, k=10)
+    X_tr, Y_tr = feature_class_split(train)
+    X_te, Y_te = feature_class_split(test)
+    tree = decision_tree(X_tr, Y_tr, max_depth, min_size)
 
-	max_depth = 5
-	min_size = 10
-
-	predicted_score = decision_tree(X_tr,X_te,max_depth,min_size)
-
-	actual = [row[-1] for row in X_tr]
+    actual = [row[-1] for row in X_tr]
 
 
 
@@ -171,15 +195,6 @@ if __name__ == '__main__':
 
 
 '''
-def feature_class_split(dataset):
-	length = len(dataset)
-	fea_len = len(dataset[0])-1
-	x = []
-	y = []
-	for i in range(length):
-		x.append(dataset[i][:-1])
-		y.append(dataset[i][fea_len])
 
-	return x, y
 
 '''
