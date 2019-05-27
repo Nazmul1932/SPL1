f99035928ff8e8eb7a00255768e7ae7822aa77be
Nazmul1932 <46520530+Nazmul1932@users.noreply.github.com>
2019-05-27 08:34:04 +0600

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -3,106 +3,6 @@ import math
 import random
 
 
-def count_class_wise_dist(Y, classes):
-    prob = [0] * len(classes)
-    if len(Y) == 0:
-        return prob
-
-    for i in range(classes):
-        class_val = classes[i]
-        prob[i] = Y.count(class_val) / len(Y)
-
-    return prob
-
-
-def claculate_entropy(prob):
-    num = len(prob)
-    entropy = 0.0
-
-    for i in range(len(prob)):
-        if prob[i] > 0:
-            entropy = entropy - (prob[i] * math.log2(prob[i]) / math.log2(num))
-    return entropy
-
-
-def find_Gini(Y_tr, Y_left, Y_right):
-    classes = set(Y_tr)
-
-    prob_tr = count_class_wise_dist(Y_tr, classes)
-    prob_left = count_class_wise_dist(Y_left, classes)
-    prob_right = count_class_wise_dist(Y_right, classes)
-
-    parent_ent = claculate_entropy(prob_tr)
-    left_ent = claculate_entropy(prob_left)
-    right_ent = claculate_entropy(prob_right)
-
-    gini = parent_ent - left_ent*len(Y_left)/len(Y_tr) - right_ent*len(Y_right)/len(Y_tr)
-
-    return gini
-
-
-def getting_Split_Dataset(X_tr, Y_tr):
-    unique_class = len(set(Y_tr))
-    if unique_class == 1:
-        return {'decision': True, 'result': Y_tr[0], 'index': '', 'value': ''}
-
-    max_gini = 1000
-    n_index = -1
-    n_value = -1
-
-    for index in range(len(X_tr[0])):
-        for row in X_tr:
-            _, _, Y_left, Y_right = split_dataset(index, row[index], X_tr, Y_tr)
-            if len(Y_right) == 0:
-                continue
-
-            gini = find_Gini(Y_tr, Y_left, Y_right)
-            if gini < max_gini:
-                n_index, n_value, n_score, n_groups = index, row[index], gini
-    return {'decision': False, 'result': '', 'index': n_index, 'value': n_value}
-
-
-def to_terminal(group):
-    outcomes = [row[-1] for row in group]
-    return max(set(outcomes), key=outcomes.count)
-
-
-def make_TREE(node, max_depth, min_size, depth):
-    left, right = node['groups']
-    del (node['groups'])
-
-    if not left or not right:
-        node['left'] = node['right'] = to_terminal(left + right)
-        return
-
-    if depth >= max_depth:
-        node['left'], node['right'] = to_terminal(left), to_terminal(right)
-        return
-
-    if len(left) <= min_size:
-        node['left'] = to_terminal(left)
-    else:
-        node['left'] = getting_Split_Dataset(left)
-        make_TREE(node['left'], max_depth, min_size, depth + 1)
-
-    if len(right) <= min_size:
-        node['right'] = to_terminal(right)
-    else:
-        node['right'] = getting_Split_Dataset(right)
-        make_TREE(node['right'], max_depth, min_size, depth + 1)
-
-
-def build_decision_TREE(X_tr, Y_tr, max_depth, min_size):
-    root = getting_Split_Dataset(X_tr, Y_tr)
-    #print(root)
-    #build_decision_TREE(X_tr, Y_tr, max_depth, min_size)
-    return root
-
-
-def decision_tree(X_train, Y_train, min_size, max_depth):
-    tree = build_decision_TREE(X_train, Y_train, max_depth, min_size)
-
-
 def load_CSV_file(file_name):
     read_file = open(file_name)
     read_lines = reader(read_file)
@@ -115,12 +15,50 @@ def str2float(dataset, column):
         row[column] = float(row[column].strip())
 
 
+def count_class_wise_dist(Y, classes):
+    prob = [0] * len(classes)
+    if len(Y) == 0:
+        return prob
+
+    index = 0
+    for class_val in classes:
+        prob[index] = Y.count(class_val) / len(Y)
+        index = index + 1
+
+    return prob
+
+
+def claculate_entropy(prob):
+    entropy = 0.0
+
+    for i in range(len(prob)):
+        if prob[i] > 0:
+            entropy = entropy - (prob[i] * math.log2(prob[i]))
+    return entropy
+
+
+def find_Info_Gain(Y_tr, Y_left, Y_right):
+    classes = set(Y_tr)
+
+    prob_tr = count_class_wise_dist(Y_tr, classes)
+    prob_left = count_class_wise_dist(Y_left, classes)
+    prob_right = count_class_wise_dist(Y_right, classes)
+
+    parent_ent = claculate_entropy(prob_tr)
+    left_ent = claculate_entropy(prob_left)
+    right_ent = claculate_entropy(prob_right)
+
+    gain = parent_ent - left_ent*len(Y_left)/len(Y_tr) - right_ent*len(Y_right)/len(Y_tr)
+
+    return gain
+
+
 def split_dataset(index, value, X, Y):
     left = []
     right = []
     Y_left, Y_right = [], []
 
-    for i in len(X):
+    for i in range(len(X)):
         row = X[i]
         if row[index] <= value:
             left.append(row)
@@ -130,6 +68,45 @@ def split_dataset(index, value, X, Y):
             Y_right.append(Y[i])
 
     return left, right, Y_left, Y_right
+
+
+
+
+
+def getting_Node(X_tr, Y_tr, current_depth, max_depth):
+    unique_class = len(set(Y_tr))
+    if unique_class == 1:
+        return {'decision': True, 'result': Y_tr[0], 'depth': current_depth, 'index': '', 'value': '', 'left': '',
+                'right': ''}
+
+    if current_depth == max_depth:
+        return {'decision': True, 'result': find_decision(Y_tr), 'depth': current_depth, 'index': '', 'value': '',
+                'left': '', 'right': ''}
+
+    max_gain = -1
+    n_index = -1
+    n_value = -1
+
+    for index in range(len(X_tr[0])):
+        for row in X_tr:
+            _, _, Y_left, Y_right = split_dataset(index, row[index], X_tr, Y_tr)
+            if len(Y_right) == 0:
+                continue
+
+            gain = find_Info_Gain(Y_tr, Y_left, Y_right)
+            if gain > max_gain:
+                n_index, n_value, max_gain = index, row[index], gain
+
+    X_left, X_right, Y_left, Y_right = split_dataset(n_index, n_value, X_tr, Y_tr)
+
+    return {'decision': False, 'result': '', 'depth': current_depth, 'index': n_index, 'value': n_value,
+            'left': getting_Node(X_left, Y_left, current_depth + 1, max_depth),
+            'right': getting_Node(X_right, Y_right, current_depth + 1, max_depth)}
+
+
+def build_decision_TREE(X_tr, Y_tr, max_depth):
+    root = getting_Node(X_tr, Y_tr, 0, max_depth)
+    return root
 
 
 def shuffle_data(dataset):
@@ -143,9 +120,21 @@ def shuffle_data(dataset):
     return dataset
 
 
+def get_unique_classes(dataset):
+    fea_len = len(dataset[0]) - 1
+    labels = []
+
+    for row in dataset:
+        labels.append(row[fea_len])
+
+    unique_classes = set(labels)
+
+    return len(unique_classes)
+
+
 def train_test_split(data_set, index, k=10):
     data_set = shuffle_data(data_set)
-    #print(str(data_set))
+
     train = []
     test = []
 
@@ -155,7 +144,7 @@ def train_test_split(data_set, index, k=10):
         else:
             train.append(data_set[i])
 
-    return train,test
+    return train, test
 
 
 def feature_class_split(dataset):
@@ -165,9 +154,44 @@ def feature_class_split(dataset):
     y = []
     for i in range(length):
         x.append(dataset[i][:-1])
-        y.append(dataset[i][fea_len])
+        y.append(int(dataset[i][fea_len]))
 
     return x, y
+
+
+def predict_label(tree, sample):
+    node = tree
+    while node['decision'] != True:
+        fea_index = node['index']
+        fea_value = node['value']
+
+        if sample[fea_index] <= fea_value:
+            node = node['left']
+        else:
+            node = node['right']
+
+    return node['result']
+
+
+def get_confusion_matrix(tree, X_te, Y_te, classes):
+    conf_mat = []
+    for i in range(classes):
+        conf_mat.append([0] * classes)
+
+    for i in range(len(X_te)):
+        Y_act = Y_te[i]
+        Y_pred = predict_label(tree, X_te[i])
+        conf_mat[Y_act][Y_pred] = conf_mat[Y_act][Y_pred] + 1
+
+    return conf_mat
+
+
+def add_conf_mat(conf_mat, conf_mat_te):
+    for i in range(len(conf_mat)):
+        for j in range(len(conf_mat[0])):
+            conf_mat[i][j] = conf_mat[i][j] + conf_mat_te[i][j]
+
+    return conf_mat
 
 
 if __name__ == '__main__':
@@ -177,16 +201,32 @@ if __name__ == '__main__':
     for i in range(len(data_set[0])):
         str2float(data_set, i)
 
-    train, test = train_test_split(data_set, 6, k=10)
-
+    classes = get_unique_classes(data_set)
+    total_sample = len(data_set)
+    conf_mat = []
     max_depth = 5
-    min_size = 10
+    k_cv = 10
 
-    X_tr, Y_tr = feature_class_split(train)
-    X_te, Y_te = feature_class_split(test)
-    tree = decision_tree(X_tr, Y_tr, max_depth, min_size)
+    for i in range(classes):
+        conf_mat.append([0] * classes)
 
-    actual = [row[-1] for row in X_tr]
+    for i in range(k_cv):
+        train, test = train_test_split(data_set, i, k=k_cv)
+
+        X_tr, Y_tr = feature_class_split(train)
+        X_te, Y_te = feature_class_split(test)
+
+        tree = build_decision_TREE(X_tr, Y_tr, max_depth)
+        conf_mat_te = get_confusion_matrix(tree, X_te, Y_te, classes)
+        conf_mat = add_conf_mat(conf_mat, conf_mat_te)
+
+    print(conf_mat)
+
+    correct = 0
+    for i in range(classes):
+        correct = correct + conf_mat[i][i]
+
+    print('Accuracy: ' + str(correct/total_sample))
 
 
 
